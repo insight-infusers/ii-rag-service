d0d95f0e0533aaad97b7b0a9caac25bb7e58e06d
ivanzhovannik <zhovannikie@gmail.com>
2024-04-17 13:27:24 +0200

    added dynaconf config + docker-compose dummy file

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-# TODO: read in settings.yml and .secret.yml here
-settings={}
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="AMV",
+    settings_files=["settings.yaml", ".secrets.yaml"],
+    merge_enabled=True,
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
