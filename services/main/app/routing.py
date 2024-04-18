2ccc7739da6458218f231e06dde86561948acf25
ivanzhovannik <zhovannikie@gmail.com>
2024-04-18 20:44:28 +0200

    cleaning main service router

--- services/main/app/routing.py
+++ services/main/app/routing.py
@@ -1,39 +1,5 @@
 # services/main/app/routing.py
 
-# import asyncio
-# import logging
-# from fastapi import APIRouter, HTTPException
-# from schema.communication import UserQueryRequest, LLMCompletionResponse
-# from core.llm.llm_engine import LLMEngine
-# from core.db.domain_specific_documents_db import DomainSpecificDocumentsDB
-# from core.db.vector_db import VectorDBEngine
-# from core.llm.embedding_engine import EmbeddingEngine
-
-# router = APIRouter()
-
-# @router.post("/query", response_model=LLMCompletionResponse)
-# async def query_llm_endpoint(query: UserQueryRequest):
-#     try:
-#         # # Retrieve embedding for the query using the EmbeddingEngine
-#         # # For simplification, we're assuming synchronous call to an async function
-#         # query_embedding = EmbeddingEngine.embed_query(query)
-
-#         # # Retrieve top k documents using the VectorDB
-#         # top_k_document_ids = VectorDBEngine.retrieve_top_k_documents(query_embedding)
-
-#         # # Get the completion from the LLM using the query and top k documents
-#         # completion = LLMEngine.generate_completion(query.query, top_k_document_ids)
-
-#         # Simulate retrieval and processing time
-#         await asyncio.sleep(2)  # Simulates network or processing delay
-#         completion = f"Processed query: {query.query}"
-
-#         return LLMCompletionResponse(completion_text=completion)
-#     except Exception as e:
-#         logger.error(f"Error processing LLM query: {e}")
-#         raise HTTPException(status_code=500, detail=str(e))
-    
-import asyncio
 import logging
 import httpx
 from fastapi import APIRouter, HTTPException
