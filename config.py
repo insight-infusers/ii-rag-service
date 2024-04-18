a5c4b1d61bb83717c98b5bb0e5ed80718fbb863b
ivanzhovannik <zhovannikie@gmail.com>
2024-04-18 10:56:21 +0200

    fixed config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="IIRAG",
-    settings_files=["settings.yml", ".secrets.yml"],
+    settings_files=["settings.yml", ".secret.yml"],
     merge_enabled=True,
 )
 
