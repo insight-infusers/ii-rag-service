30d5f376d7739fe7a3f957074f6aca9321e97347
ivanzhovannik <zhovannikie@gmail.com>
2024-04-18 19:03:31 +0200

    added startup logs

--- services/retriever/app/main.py
+++ services/retriever/app/main.py
@@ -4,3 +4,8 @@ from config import settings
 
 app = FastAPI(title=settings.title, version=settings.version)
 app.include_router(router)
+
+@app.on_event("startup")
+async def startup_event():
+    print("Starting up, registering routes...")
+    print(app.routes)  # This will print out all registered routes
